f5a4bf703c0ca42e7b76c5aa5894660f939d49ba
tsheporapuleng <trapuleng9248@gmail.com>
2024-08-13 18:26:50 +0200

    fixing navifation bar

--- lib/shared/views/bottomNavigation_bar.dart
+++ lib/shared/views/bottomNavigation_bar.dart
@@ -8,28 +8,20 @@ import '../../feature/cart/presentation/views/cart_page.dart';
 
 
 class BottomNavBar extends StatelessWidget {
+
   const BottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<BottomNavBloc, BottomNavState>(
       listener: (context, state) {
-        if(state.tabIndex == 1){
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const CartPage(),
-            ),
-          );
-        }
       },
       builder: (context, state) {
         return ClipRRect(
-
           borderRadius: const BorderRadius.vertical(top: Radius.circular(30.0)),
           child: BottomNavigationBar(
             onTap: (index) {
               context.read<BottomNavBloc>().add(SelectedTabEvent(index));
-              print(index);
             },
             currentIndex
             : state.tabIndex,
@@ -38,9 +30,8 @@ class BottomNavBar extends StatelessWidget {
             fixedColor: Colors.red,
             elevation: 3,
             items: const [
-
               BottomNavigationBarItem(
-                  label: "home", icon: Icon(Icons.home)),
+                  label: "home", icon: Icon(Icons.home_outlined)),
               BottomNavigationBarItem(
                   label: "home", icon: Icon(Icons.add_shopping_cart_rounded))
             ],
